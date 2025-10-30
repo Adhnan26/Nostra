7dc5b7b02911a38f32b8a986cc2e6fb43f75b68a
Adhnan26 <mhd.adhnan26@gmail.com>
2025-10-30 20:57:08 +0530

    Updated js

--- contactus.js
+++ contactus.js
@@ -18,6 +18,77 @@ document.getElementById("side-navbar-close").addEventListener("click", () => {
 
 
 
+// Textarea limit
+const form = document.getElementById("form")
+const message = document.getElementById("message")
+const charcount = document.getElementById("charCount")
+
+message.addEventListener("input", () => {
+    if (message.value.length > 300) {
+        message.value = message.value.substring(0, 300);
+    }
+
+    const length = message.value.length; 
+    charcount.textContent = `${length} / 300 characters`;
+});
+
+
+// Regex Error Message
+const submit = document.getElementById("submit")
+
+submit.addEventListener("click", (event) => {
+    event.preventDefault()
+
+    let validation = true
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+
+    const Regexname = /^[a-zA-Z ]+$/
+    const Regexemail = /^[a-zA-Z0-9]+@gmail\.com$/
+
+    const nameError = document.querySelector(".nameError")
+    const emailError = document.querySelector(".emailError")
+
+
+
+    if (!Regexname.test(name)) {
+        nameError.style.display = "inline"
+        validation = false
+    }
+
+    else {
+        nameError.style.display = "none"
+    }
+
+    if (!Regexemail.test(email)) {
+        emailError.style.display = "inline"
+        validation = false
+    }
+
+    else {
+        emailError.style.display = "none"
+    }
+
+    if (message.value.trim().length == 0) {
+        alert("Message cannot be empty");
+        validation = false;
+    }
+
+    if (validation == true) {
+        alert("Submitted Successfully")
+        form.reset();
+        charcount.textContent = "0 / 300 characters"
+        nameError.style.display = "none"
+        emailError.style.display = "none"
+    }
+
+})
+
+
+
+
+
 
 
 
